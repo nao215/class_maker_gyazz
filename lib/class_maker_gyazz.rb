eb05ecb9bd7524e4e6756c479eb41ea6273da76e
nao215 <xxxxxy.naoxxxxx@gmail.com>
2016-01-14 23:28:03 +0900

    generate li and ul tags

--- lib/class_maker_gyazz.rb
+++ lib/class_maker_gyazz.rb
@@ -1,5 +1,28 @@
 require "class_maker_gyazz/version"
 
 module ClassMakerGyazz
-  # Your code goes here...
+
+  class Make
+
+    attr_accessor :html
+    
+    def initialize text
+      before_depth, current_depth, next_depth = 0
+      rows = text.split("\n")
+      html = rows.map.with_index do |m, i|
+        puts "current #{current_depth}, next_depth #{next_depth}, before_depth #{before_depth}"
+        prefix = "<li>"
+        suffix = "</li>"
+        current_depth = m.length - m.lstrip.length
+        next_depth = rows[i + 1].length - rows[i + 1].lstrip.length unless rows.last == m
+        suffix = "" if next_depth > current_depth # next row will start nest
+        prefix = "<ul><li>" if before_depth < current_depth # nest start
+        suffix = "#{suffix}#{'</li></ul>' * (current_depth - next_depth)}" if current_depth > next_depth # nest end
+        before_depth = current_depth
+        "#{prefix}#{m}#{suffix}"
+      end
+      @html = "<ul>#{html.join}</ul>"
+    end
+  end
+
 end
--- spec/class_maker_gyazz_spec.rb
+++ spec/class_maker_gyazz_spec.rb
@@ -5,7 +5,15 @@ describe ClassMakerGyazz do
     expect(ClassMakerGyazz::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'normally outputted from gyazz' do
+    require 'pp'
+    text = File.open(File.expand_path('../gyazz_texts/test.gyazz', __FILE__)) do |file| 
+      file.read
+    end
+    file_path = File.expand_path("../html/" + "gyazz_html#{Time.now}.html".gsub("\/","\|").gsub(" ","_"), __FILE__)
+    File.write(file_path, ClassMakerGyazz::Make.new(text).html)
+    puts file_path
+    system "open #{file_path}"
+    expect(true).to eq(true)
   end
 end
