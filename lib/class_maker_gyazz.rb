038a81500262c3ef3fda85bfefa9b77fdaf62658
nao215 <xxxxxy.naoxxxxx@gmail.com>
2016-01-18 00:05:02 +0900

    img,link,bold text

--- lib/class_maker_gyazz.rb
+++ lib/class_maker_gyazz.rb
@@ -7,10 +7,9 @@ module ClassMakerGyazz
     attr_accessor :html
     
     def initialize text
-      before_depth, current_depth, next_depth = 0
+      before_depth = current_depth = next_depth = 0
       rows = text.split("\n")
       html = rows.map.with_index do |m, i|
-        puts "current #{current_depth}, next_depth #{next_depth}, before_depth #{before_depth}"
         prefix = "<li>"
         suffix = "</li>"
         current_depth = m.length - m.lstrip.length
@@ -19,10 +18,47 @@ module ClassMakerGyazz
         prefix = "<ul><li>" if before_depth < current_depth # nest start
         suffix = "#{suffix}#{'</li></ul>' * (current_depth - next_depth)}" if current_depth > next_depth # nest end
         before_depth = current_depth
-        "#{prefix}#{m}#{suffix}"
+        tag, end_tag = text_tag(m, current_depth, next_depth)
+        "#{prefix}#{tag}#{check_text(m)}#{end_tag}#{suffix}"
       end
       @html = "<ul>#{html.join}</ul>"
     end
+
+    def text_tag text, current_depth, next_depth
+      if current_depth == 0
+        tag = is_img?(text) ? "<h2>" : "<h2 class='page-header'>" 
+        end_tag = "</h2>"
+        return tag, end_tag
+      end
+    end
+
+    def check_text text
+      if is_img?(text)
+        text = "<img class='img-thumbnail' src='#{clean_brackets(text)}'>"
+      elsif text =~ /\[\[\[.*\]\]\]/
+        text = "<strong>#{text}</strong>"
+        text = check_link(clean_brackets(text))
+      elsif text =~ /\[\[.*\]\]/
+        text = check_link(clean_brackets(text))
+      end
+      text
+    end
+
+    def check_link text
+      if text =~ /(http|https)\:\/\/\w.*/
+        text = "#{text.gsub($&,"")}<a href='#{$&}'>#{$&}</a>"
+      end
+      text
+    end
+
+    def is_img? text
+      text =~ /\[\[(http|https)\:\/\/\w.*(png|jpg|jpeg|gif)\]\]/
+    end
+
+    def clean_brackets text
+      text.gsub(/(\[\[\[|\]\]\])/, "").gsub(/(\[\[|\]\])/, "")
+    end
+
   end
 
 end
--- spec/class_maker_gyazz_spec.rb
+++ spec/class_maker_gyazz_spec.rb
@@ -11,7 +11,18 @@ describe ClassMakerGyazz do
       file.read
     end
     file_path = File.expand_path("../html/" + "gyazz_html#{Time.now}.html".gsub("\/","\|").gsub(" ","_"), __FILE__)
-    File.write(file_path, ClassMakerGyazz::Make.new(text).html)
+    html = """
+    <html>
+      <head>
+        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+      </head>
+      <body>
+        #{ClassMakerGyazz::Make.new(text).html}
+      </body>
+    </html>
+    """
+    File.write(file_path, html)
     puts file_path
     system "open #{file_path}"
     expect(true).to eq(true)
